9785b6ddf6ed85d2703f4dececa63ebbf72feb16
hamza202 <k8668975@gmail.com>
2020-09-24 17:27:42 +0300

    home page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $(".close-search").on('click', function () {
     $('.header-search').fadeOut(200)
 });
@@ -6,10 +5,10 @@ $(".header-search-button").on('click', function () {
     $('.header-search').slideToggle(200)
 });
 
-if($('#player').length){
-    const player = new Plyr('#player',{
-        speed: { selected: 1, options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2] },
-        quality:{ default: 720, options: [1080, 720, 480, 360] }
+if ($('#player').length) {
+    const player = new Plyr('#player', {
+        speed: {selected: 1, options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]},
+        quality: {default: 720, options: [1080, 720, 480, 360]}
     });
     player.on('play', () => {
         $('.video-section h3').fadeOut(500);
@@ -26,9 +25,18 @@ $(window).scroll(function () {
     if (scroll >= 200) {
         sticky.addClass('fixed');
         $("body").addClass("headerIsFixed")
-    }
-    else {
+    } else {
         sticky.removeClass('fixed');
         $("body").removeClass("headerIsFixed")
     }
+});
+
+
+let socialBox = $('.our-social a');
+let divWidth = socialBox.width();
+
+socialBox.height(divWidth - 10);
+$(window).on("resize" ,function () {
+    socialBox.height(divWidth - 10);
+    console.log("hamza")
 });
